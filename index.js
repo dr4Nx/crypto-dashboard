0ab7322274f2c14f824bff85aed07737e123bfc1
Evan L <eluo2015@gmail.com>
2024-10-02 23:56:01 -0700

    add history

--- index.js
+++ index.js
@@ -9,33 +9,38 @@ const port = process.env.PORT || 3000;
 const api_url = "https://pro-api.coinmarketcap.com";
 
 var api_key = process.env.API_KEY || "YOUR_API_KEY";
+var ids = ['1'];
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const crypto_list_response = await axios.get(`${api_url}/v1/cryptocurrency/listings/latest`, {
-  headers: {
-    'X-CMC_PRO_API_KEY': api_key
-  }
-});
-
-const crypto_list = crypto_list_response.data.data;
+var crypto_list_response;
 
 app.get('/', async (req, res) => {
   try {
     const id = req.query.crypto_select || 1;
-    const content = await axios.get(`${api_url}/v2/cryptocurrency/quotes/latest?id=${id}`, {
+    const idIndex = ids.indexOf(id);
+    if (idIndex !== -1) {
+      ids.splice(idIndex, 1);
+    }
+    ids.unshift(id);
+    console.log(ids);
+    const content = await axios.get(`${api_url}/v2/cryptocurrency/quotes/latest?id=${ids.join(',')}`, {
+      headers: {
+      'X-CMC_PRO_API_KEY': api_key
+      }
+    });
+    crypto_list_response = await axios.get(`${api_url}/v1/cryptocurrency/listings/latest`, {
       headers: {
         'X-CMC_PRO_API_KEY': api_key
       }
-    }
-    );
-    res.render("index.ejs", { api_key: api_key, id_list: crypto_list, data: content.data.data[`${id}`] });
+    });
+    const crypto_list = crypto_list_response.data.data;
+    res.render("index.ejs", { api_key: api_key, id_list: crypto_list, data: content.data.data, ids: ids });
   } catch (err) {
-    res.sendStatus(500);
+    res.render("index.ejs", { api_key: api_key });
     console.error(err);
   }
-  
 });
 
 app.post('/set-api-key', async (req, res) => {
